76d9f90215628381a26a7cd0c158999b370e2cee
Jonas Harnau <j.harnau@outlook.com>
2018-06-20 09:02:23 -0400

    docs: Adjust version number in setup.py 0.2.0 -> 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='apc',
-      version='0.2.0',
+      version='0.3.0',
       description='Age-Period-Cohort Analysis',
       url='http://github.com/JonasHarnau/apc',
       author='Jonas Harnau',
