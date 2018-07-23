ae5c7cf5709e38662618f2de58a92d82219607b2
Jonas Harnau <j.harnau@outlook.com>
2018-07-23 18:01:36 -0400

    tests: Add test_plot_fit

---
+++ apc/tests/test_plot_fit.py
@@ -0,0 +1,34 @@
+import unittest
+import apc
+import matplotlib.pyplot as plt
+
+class TestPlotFit(unittest.TestCase):
+
+    def test_BZ(self):
+        model = apc.Model()
+        model.data_from_df(apc.asbestos(), data_format='CL')
+        model.fit('od_poisson_response', 'AC')
+        model.plot_fit()
+        for style in ('detrend', 'sum_sum'):
+            model.plot_fit(style)
+        plt.close('all')
+        for sr in ('start', 'mean', 'end', False):
+            model.plot_fit(simplify_ranges=sr)
+        plt.close('all')
+        model.plot_fit(around_coef=False)
+        
+    def test_Belgian(self):
+        model = apc.Model()
+        model.data_from_df(**apc.Belgian_lung_cancer())
+        model.fit('log_normal_rates', 'APC')
+        model.plot_fit()
+        for style in ('detrend', 'sum_sum'):
+            model.plot_fit(style)
+        plt.close('all')
+        for sr in ('start', 'mean', 'end', False):
+            model.plot_fit(simplify_ranges=sr)
+        plt.close('all')
+        model.plot_fit(around_coef=False)
+        
+if __name__ == '__main__':
+    unittest.main()
