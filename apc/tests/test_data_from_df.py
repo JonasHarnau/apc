ac2cf440a35b00898dc3f1d1d6a0476e4a6f6590
Jonas Harnau <j.harnau@outlook.com>
2018-07-22 22:49:37 -0400

    test: Expand test_data_from_df

--- apc/tests/test_data_from_df.py
+++ apc/tests/test_data_from_df.py
@@ -4,9 +4,21 @@ import apc
 
 class TestDataFromDf(unittest.TestCase):
 
+    def test_asbestos(self):
+        model = apc.Model()
+        model.data_from_df(apc.asbestos())
+        
+        self.assertEqual(model.data_format, 'PA')
+        self.assertEqual(model.I, 70)
+        self.assertEqual(model.J, 41)
+        self.assertEqual(model.K, 110)
+        self.assertEqual(model.L, 69)
+        self.assertEqual(model.n, 2870)
+        self.assertEqual(model.data_vector.sum()['response'], 32164)    
+    
     def test_TA(self):
         model = apc.Model()
-        model.data_from_df(apc.loss_TA(), time_adjust=1)
+        model.data_from_df(apc.loss_TA())
         
         self.assertEqual(model.data_format, 'CA')
         self.assertEqual(model.I, 10)
@@ -14,8 +26,23 @@ class TestDataFromDf(unittest.TestCase):
         self.assertEqual(model.K, 10)
         self.assertEqual(model.L, 0)
         self.assertEqual(model.n, 55)
+        self.assertEqual(model.time_adjust, 1)
         self.assertEqual(round(model.data_vector.sum()['response'],3), 
                          34358090.0)
+
+    def test_BZ(self):
+        model = apc.Model()
+        model.data_from_df(apc.loss_BZ(), data_format='CL')
+        
+        self.assertEqual(model.data_format, 'CL')
+        self.assertEqual(model.I, 11)
+        self.assertEqual(model.J, 11)
+        self.assertEqual(model.K, 11)
+        self.assertEqual(model.L, 0)
+        self.assertEqual(model.n, 66)
+        self.assertEqual(model.time_adjust, 0)
+        self.assertEqual(round(model.data_vector.sum()['response'],3), 
+                         10221194.0)        
         
     def test_Belgian(self):
         data = pd.read_excel('./apc/data/Belgian_lung_cancer.xlsx', 
