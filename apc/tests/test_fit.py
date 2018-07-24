dc218364c4fbd003b8e1651671e3a72afc1d2f30
Jonas Harnau <j.harnau@outlook.com>
2018-07-24 12:43:10 -0400

    test: Add 'poisson_dose_response' to test_fit

--- apc/tests/test_fit.py
+++ apc/tests/test_fit.py
@@ -85,6 +85,27 @@ class TestFit(unittest.TestCase):
         self.assertEqual(model.fitted_values.sum().astype(int), 
                          model.data_vector.sum()[0])
         
+    def test_Belgian_poisson_dose_response(self):
+        model = apc.Model()
+        model.data_from_df(**apc.Belgian_lung_cancer())
+        model.fit(family='poisson_dose_response', predictor='APC')
+        
+        self.assertEqual(round(model.deviance,3), 20.225)
+        self.assertTrue(np.allclose(
+            model.para_table['P>|z|'].values,
+            np.array([
+                4.98090007e-194, 2.00792411e-011, 7.54393422e-002, 2.44873634e-001,
+                3.78642275e-001, 4.49590657e-001, 1.71890762e-001, 7.15051524e-001,
+                3.40338551e-001, 7.77531241e-001, 5.43258659e-001, 3.10146699e-001,
+                3.27420794e-001, 3.02443187e-001, 4.90579409e-001, 8.10857155e-001,
+                8.99207007e-001, 2.56294262e-001, 4.15989929e-001, 9.55962845e-001,
+                9.06764652e-001, 5.55322937e-001, 3.42813423e-001, 4.50710269e-001,
+                7.13277215e-001, 4.54604834e-001
+            ]), 
+            equal_nan=True)
+                       )
+        self.assertEqual(round(model.fitted_values.sum(),3), 6092.0)
+        
     def test_Belgian_ln_rates(self):
         model = apc.Model()
         model.data_from_df(**apc.Belgian_lung_cancer())
