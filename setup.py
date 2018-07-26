6c694318e4030367c2c34d16b529b8b3ef59b6d0
Jonas Harnau <j.harnau@outlook.com>
2018-07-26 17:46:22 -0400

    chore: Update to setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+import setuptools
 
-setup(name='apc',
-      version='1.0.0',
-      description='Age-Period-Cohort and extended Chain-Ladder Analysis',
-      url='http://github.com/JonasHarnau/apc',
-      author='Jonas Harnau',
-      author_email='j.harnau@outlook.com',
-      license='GPLv3',
-      packages=['apc'],
-      install_requires=['matplotlib', 'numpy',
-                        'pandas', 'scipy',
-                        'seaborn', 'statsmodels',
-                        'quad_form_ratio'],
-      python_requires='>=3.6',
-      include_package_data=True,
-      classifiers=['Development Status :: 3 - Alpha'],
-      long_description=open('README.rst').read(),
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='apc',
+    version='1.0.0',
+    description='Age-Period-Cohort and extended Chain-Ladder Analysis',
+    url='http://github.com/JonasHarnau/apc',
+    author='Jonas Harnau',
+    author_email='j.harnau@outlook.com',
+    license='GPLv3',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    include_package_data=True,
+    classifiers=['Development Status :: 3 - Alpha'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False)
