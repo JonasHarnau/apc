ce2a3b030d229d9736ea25fe47b9d79690d427fc
Jonas Harnau <j.harnau@outlook.com>
2018-07-25 22:12:38 -0400

    test: Update test_bootstrap_forecast

--- apc/tests/test_bootstrap_forecast.py
+++ apc/tests/test_bootstrap_forecast.py
@@ -5,14 +5,11 @@ import apc
 class TestCLBootstrap(unittest.TestCase):
 
     def test_TA(self):
-        model = apc.Model()
-        model.data_from_df(apc.loss_TA(), data_format='CL')
-        model.fit('od_poisson_response', 'AC')
-        apc.bootstrap_forecast(model, B=10)
-        apc.bootstrap_forecast(model, B=10, quantiles=[0.9])
-        apc.bootstrap_forecast(model, B=10, adj_residuals=False)
-        f1 = apc.bootstrap_forecast(model, B=10, seed=1)
-        f2 = apc.bootstrap_forecast(model, B=10, seed=1)
+        apc.bootstrap_forecast(apc.loss_TA(), B=10)
+        apc.bootstrap_forecast(apc.loss_TA(), B=10, quantiles=[0.9])
+        apc.bootstrap_forecast(apc.loss_TA(), B=10, adj_residuals=False)
+        f1 = apc.bootstrap_forecast(apc.loss_TA(), B=10, seed=1)
+        f2 = apc.bootstrap_forecast(apc.loss_TA(), B=10, seed=1)
         for key in f1.keys():
             self.assertTrue(np.allclose(f1[key].values, f2[key].values))
 
