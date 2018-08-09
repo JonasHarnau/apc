d81bc78a23bcf8471a13a3b77c115ea25644f0f7
Jonas Harnau <j.harnau@outlook.com>
2018-08-09 18:27:14 -0400

    Chore: Add dependencies to setup.py and update version

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+"""apc: Age-Period-Cohort and extended Chain-Ladder Analysis."""
+
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='apc',
-    version='1.0.0',
+    version='1.0.1',
     description='Age-Period-Cohort and extended Chain-Ladder Analysis',
     url='http://github.com/JonasHarnau/apc',
     author='Jonas Harnau',
@@ -13,6 +15,10 @@ setuptools.setup(
     license='GPLv3',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
+    install_requires=[
+        'matplotlib', 'numpy', 'pandas', 'quad_form_ratio', 'seaborn',
+        'scipy', 'statsmodels'
+    ],
     include_package_data=True,
     classifiers=['Development Status :: 3 - Alpha'],
     long_description=long_description,
